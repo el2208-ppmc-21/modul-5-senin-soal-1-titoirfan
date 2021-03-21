2a0260366ccd3af76243035d2df23ca76320b167
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-03-21 23:37:02 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -1,21 +1,51 @@
-/** EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul               : 5
-*   Soal                : 1
-*   Hari dan Tanggal    : Senin, 22 Maret 2021
-*   Nama (NIM)          : 
-*   Asisten (NIM)       : 
-*   Nama File           : soal-01.c
-*   Deskripsi           : Deskripsi file ini.
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    // Tulis kode anda di sini
-    
-    // Format printf untuk autograder
-    // printf("Jumlah ubin minimum: %d", jumlah_ubin_minimum);
-    
-    return 0;
+int isEven(int x)
+{
+    if (x % 2 == 0)
+        return 1;
+    else
+        return 0;
+}
+
+int tileCount(int m, int n)
+{
+    if (n == 0 || m == 0)
+    {
+        return 0;
+    }
+
+    else if (isEven(m) && isEven(n))
+    {
+        return tileCount(m / 2, n / 2); // kalo sama sama genap, berarti jumlah tile sama aja kayak kalo ukuran dibagi 2
+                                        // contoh: jumlah ubin untuk 10,10 == jumlah ubin untuk 5,5
+    }
+
+    else if (isEven(m) && !isEven(n))
+    {
+        return m + tileCount(m / 2, n / 2); // contoh: 6x5; di sisi paling kiri diisi penuh 1x1
+                                            // jadi 5 + tileCount(6, 4),
+                                            // karena sama sama genap jadi 5 + tileCount(3,2)
+    }
+
+    else if (!isEven(m) && isEven(n))
+    {
+        return n + tileCount(m / 2, n / 2); // ^sama
+    }
+
+    else
+        return n + m - 1 + tileCount(m / 2, n / 2); // Sama kayak satu ganjil, tapi ini sekeliling ukuran genap
+                                                    // di isi 1x1 jadi n+m-1
+}
+
+int main()
+{
+    int m, n;
+
+    printf("Masukkan panjang ruangan: ");
+    scanf("%d", &m);
+    printf("Masukkan lebar ruangan: ");
+    scanf("%d", &n);
+
+    printf("Jumlah ubin minimum: %d", tileCount(m, n));
 }
